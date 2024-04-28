8aa3b28050937f6243bda3d9d65b41062ff0605a
blacko7 <bgyawu@gmail.com>
2024-04-28 07:59:51 -0700

    update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
-# Executes a bash command
-exec { 'pkill killmenow':
-	path => '/usr/bin:/usr/sbin:/bin'
+# execute pkill command to kill bashscript file killmenow
+exec { 'pkill':
+  command => 'pkill -9 -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
